7665a74e996d8ce00bdb25b2f320609af31995fa
Monika Dabrowska <monikad71@onet.pl>
2023-01-14 19:14:26 +0100

    Solution 14.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,10 +15,32 @@ public class Main {
     }
 
     void run(Scanner scanner) {
-        Map<String, HashSet<Country>> countriesMap = CountryApp.readFile();
+        Map<String, HashSet<Country>> countriesMap = readFile();
         System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
         String userCountryCode = scanner.nextLine();
         printCountryStats(countriesMap, userCountryCode);
+    }
+
+    static Map<String, HashSet<Country>> readFile() {
+        String fileName = "countries.csv";
+        Map<String, HashSet<Country>> countries = new HashMap<>();
+        try {
+            Scanner scan = new Scanner(new File(fileName));
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] split = line.split(";");
+                String code = split[0];
+                BigInteger population = (new BigInteger(split[2]));
+                Country country = new Country(split[0], split[1], population);
+
+                HashSet<Country> codeSet = new HashSet<>();
+                codeSet.add(country);
+                countries.put(code, codeSet);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku countries.csv.");
+        }
+        return countries;
     }
 
     private void printCountryStats(Map<String, HashSet<Country>> countries, String userCountryCode) {
